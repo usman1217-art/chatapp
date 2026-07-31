4596a43e3df5c31c0ea98a51e0dfed7230b0f0d0
Usman <ua031860@gmail.com>
2026-07-31 17:09:42 +0500

    new commit for privcy issues

--- Frontend/src/components/common/GoogleAuthButton.jsx
+++ Frontend/src/components/common/GoogleAuthButton.jsx
@@ -26,7 +26,7 @@ function GoogleAuthButton() {
       );
     }
   };
-console.log(import.meta.env.VITE_GOOGLE_CLIENT_ID)  
+
   if (!import.meta.env.VITE_GOOGLE_CLIENT_ID) {
     return (
       <button
