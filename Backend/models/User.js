d72a8b73de88949c540de47cccab647e7ceb269d
Usman <ua031860@gmail.com>
2026-08-21 11:48:58 +0500

    google register added

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -21,7 +21,9 @@ const userSchema = new mongoose.Schema(
 
     password: {
       type: String,
-      required: true,
+      required: function () {
+        return !this.isGoogleUser;
+      },
     },
 
     avatar: {
